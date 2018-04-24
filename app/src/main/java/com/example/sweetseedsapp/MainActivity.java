489734f893467dd7ad4a5f2a9e72a4aab0b33962
Vivian Babirye Kulumba <vivianbabiryekulumba@Vivians-MBP.home>
2018-04-24 14:20:09 -0400

    fragments selectable

--- app/src/main/java/com/example/sweetseedsapp/MainActivity.java
+++ app/src/main/java/com/example/sweetseedsapp/MainActivity.java
@@ -2,36 +2,52 @@ package com.example.sweetseedsapp;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationItemView;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationItemView = findViewById(R.id.navigationView);
-        bottomNavigationItemView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.navigation_home:
-                        HomeFragment.newInstance();
-                        break;
-                    case R.id.navigation_stats:
-                        StatsFragment.newInstance();
-                        break;
-                    case R.id.navigation_games:
-                        GamesFragment.newInstance();
-                        break;
-                }
-                return false;
-            }
-        });
+        bottomNavigationView = findViewById(R.id.navigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                selectedFragment = HomeFragment.newInstance();
+                                break;
+
+                            case R.id.navigation_stats:
+                                selectedFragment = StatsFragment.newInstance();
+                                break;
+
+                            case R.id.navigation_games:
+                                selectedFragment = GamesFragment.newInstance();
+                                break;
+                        }
+
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, HomeFragment.newInstance());
+        transaction.commit();
+
+
     }
 }
