76c33a5b43180f2fe76e77a411703591493df4f1
Vivian Babirye Kulumba <vivianbabiryekulumba@Vivians-MBP.home>
2018-05-20 19:13:12 -0400

    Logical activity Terni Lapiilli set board

--- app/src/main/java/com/example/sweetseedsapp/activities/GamesActivity.java
+++ app/src/main/java/com/example/sweetseedsapp/activities/GamesActivity.java
@@ -1,16 +1,12 @@
 package com.example.sweetseedsapp.activities;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-
 import com.example.sweetseedsapp.R;
 import com.example.sweetseedsapp.controllersandviews.GamesAdapter;
 import com.example.sweetseedsapp.models.GamesModel;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +27,6 @@ public class GamesActivity extends AppCompatActivity {
         GamesAdapter gamesAdapter = new GamesAdapter(populateGamesActivity());
         gamesAdapter.notifyDataSetChanged();
         recyclerView.setAdapter(gamesAdapter);
-
     }
 
     private List<GamesModel> populateGamesActivity() {
--- app/src/main/java/com/example/sweetseedsapp/activities/LogicalActivity.java
+++ app/src/main/java/com/example/sweetseedsapp/activities/LogicalActivity.java
@@ -2,14 +2,29 @@ package com.example.sweetseedsapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.example.sweetseedsapp.R;
+import com.example.sweetseedsapp.controllersandviews.GamesAdapter;
+import com.example.sweetseedsapp.controllersandviews.GamesGridViewAdapter;
 
 public class LogicalActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logical);
+
+        GridView mGridView = findViewById(R.id.logical_tx);
+        mGridView.setAdapter(new GamesGridViewAdapter());
+
+
     }
+
+
 }
