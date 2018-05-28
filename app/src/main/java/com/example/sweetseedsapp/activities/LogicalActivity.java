6269e217155549ba818f08c950f1526ceb4779ff
ViviKulu <babimaji1111@gmail.com>
2018-05-28 19:31:54 -0400

    drag and drop set in terni lapilli

--- app/src/main/java/com/example/sweetseedsapp/activities/LogicalActivity.java
+++ app/src/main/java/com/example/sweetseedsapp/activities/LogicalActivity.java
@@ -1,172 +1,88 @@
 package com.example.sweetseedsapp.activities;
 
-import android.annotation.TargetApi;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.sweetseedsapp.R;
 import com.example.sweetseedsapp.controllersandviews.DragListener;
 import com.example.sweetseedsapp.controllersandviews.LongPressListener;
-import com.example.sweetseedsapp.models.LogicalModel;
-
-import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class LogicalActivity extends AppCompatActivity {
 
     private static final String TAG = "LogicalActivity";
 
-    //Views for stones and clubs or "X" and "O".
-    private static ImageView stone1;
-    private static ImageView stone2;
-    private static ImageView stone3;
-    private static ImageView stone4;
-    private static ImageView stone5;
-    private static ImageView club1;
-    private static ImageView club2;
-    private static ImageView club3;
-    private static ImageView club4;
-    private static ImageView club5;
+    private GridLayout gridLayout;
+    private LinearLayout linearLayout;
+
+    private static LinearLayout ll1;
+    private static LinearLayout ll2;
+    private static LinearLayout ll3;
+    private static LinearLayout ll4;
+    private static LinearLayout ll5;
+    private static LinearLayout ll6;
+    private static LinearLayout ll7;
+    private static LinearLayout ll8;
+    private static LinearLayout ll9;
 
     //View and type text for round text at the top.
     private static TextView round_text;
     Typeface typeface;
 
-    //For the textViews inside of the boardViews.
-//    private static String[] boardNumbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
-    private static int[] boardViews = {R.id.image_tv1, R.id.image_tv2, R.id.image_tv3, R.id.image_tv4, R.id.image_tv5, R.id.image_tv6, R.id.image_tv7, R.id.image_tv8, R.id.image_tv9};
-
     //Used for determining if the game is over
-    private static int[][] possibleWinningCombo = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {6, 4, 2}};
-
-    //To pass in game pieces as one data structure
-    private static int[] gamePieces = {R.id.stone1, R.id.stone2, R.id.stone3, R.id.stone4, R.id.stone5, R.id.club1, R.id.club2, R.id.club3, R.id.club4, R.id.club5};
-
-    private static int currentState;  // the current state of the game
-    private static String currentPlayer; // the current player
+//    private static int[][] possibleWinningCombo = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {6, 4, 2}};
+    private static int[] boardPieces = {R.id.topleft, R.id.topmiddle, R.id.topright,
+            R.id.middleleft, R.id.middle, R.id.middleright, R.id.bottomleft, R.id.middlebottom, R.id.bottomright};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logical);
 
+        gridLayout = findViewById(R.id.grid_layout);
+        linearLayout = findViewById(R.id.linear_layout);
+
         //To set the text type.
         typeface = Typeface.createFromAsset(getAssets(), "fonts/edo.ttf");
 
-        //Setting the views and their drag and drop.
-        stone1 = findViewById(R.id.stone1);
-        stone1.setOnLongClickListener(new LongPressListener());
-        stone1.setOnDragListener(new DragListener());
+        ll1 = findViewById(R.id.topleft);
+        ll2 = findViewById(R.id.topmiddle);
+        ll3 = findViewById(R.id.topright);
+        ll4 = findViewById(R.id.middleleft);
+        ll5 = findViewById(R.id.middle);
+        ll6 = findViewById(R.id.middleright);
+        ll7 = findViewById(R.id.bottomleft);
+        ll8 = findViewById(R.id.middlebottom);
+        ll9 = findViewById(R.id.bottomright);
 
-        stone2 = findViewById(R.id.stone2);
-        stone2.setOnLongClickListener(new LongPressListener());
-        stone2.setOnDragListener(new DragListener());
+        TypedArray arrayResources = getResources().obtainTypedArray(
+                R.array.resicon);
 
-        stone3 = findViewById(R.id.stone3);
-        stone3.setOnLongClickListener(new LongPressListener());
-        stone3.setOnDragListener(new DragListener());
-
-        stone4 = findViewById(R.id.stone4);
-        stone4.setOnLongClickListener(new LongPressListener());
-        stone4.setOnDragListener(new DragListener());
-
-        stone5 = findViewById(R.id.stone5);
-        stone5.setOnLongClickListener(new LongPressListener());
-        stone5.setOnDragListener(new DragListener());
-
-        club1 = findViewById(R.id.club1);
-        club1.setOnLongClickListener(new LongPressListener());
-        club1.setOnDragListener(new DragListener());
-
-        club2 = findViewById(R.id.club2);
-        club2.setOnLongClickListener(new LongPressListener());
-        club2.setOnDragListener(new DragListener());
-
-        club3 = findViewById(R.id.club3);
-        club3.setOnLongClickListener(new LongPressListener());
-        club3.setOnDragListener(new DragListener());
-
-        club4 = findViewById(R.id.club4);
-        club4.setOnLongClickListener(new LongPressListener());
-        club4.setOnDragListener(new DragListener());
-
-        club5 = findViewById(R.id.club5);
-        club5.setOnLongClickListener(new LongPressListener());
-        club5.setOnDragListener(new DragListener());
+        ll1.setOnDragListener(new DragListener());
+        ll2.setOnDragListener(new DragListener());
+        ll3.setOnDragListener(new DragListener());
+        ll4.setOnDragListener(new DragListener());
+        ll5.setOnDragListener(new DragListener());
+        ll6.setOnDragListener(new DragListener());
+        ll7.setOnDragListener(new DragListener());
+        ll8.setOnDragListener(new DragListener());
+        ll9.setOnDragListener(new DragListener());
 
         round_text = findViewById(R.id.round_text);
         round_text.setTypeface(typeface);
 
-
-
-        //Initiate the game by getting the human spot.
-        //If the game is not over and it's not a tie, evaluate the board.
-        //Repeat if the game is not over.
-        initGame();
-
-//        do {
-//            if (!gameOver() && !tie()) {
-//                evalBoard();
-//            }
-//        } while (!gameOver() && !tie());
-        System.out.print("Game over\n");
+        for (int i = 0; i < arrayResources.length(); i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setImageDrawable(arrayResources.getDrawable(i));
+            imageView.setOnLongClickListener(new LongPressListener());
+            linearLayout.addView(imageView);
+        }
 
     }
-
-
-    private static void initGame() {
-        currentState = 0; // "playing" or ready to play
-        currentPlayer = "X";  // clubs plays first
-        Log.d(TAG, "initGame: " + currentPlayer + currentState);
-    }
-
-
-//    private static void evalBoard() {
-//        boolean foundSpot;
-//
-//        do {
-//            if (board[4].equals("4")) {
-//                board[4] = "O";
-//                foundSpot = true;
-//            } else {
-//                int spot = getBestMove();
-//                if (spot != -1 && (!board[spot].equals("X") && !board[spot].equals("O"))) {
-//                    foundSpot = true;
-//                    board[spot] = "O";
-//                } else {
-//                    foundSpot = false;
-//                }
-//            }
-//        } while (!foundSpot);
-//        printBoard();
-//    }
-
-//    private static boolean gameOver() {
-//        //When the currentState is 9 or the same length of the boardNumbers.
-//        return boardViews[0] == (boardViews[1]) && boardViews[1] == (boardViews[2]) ||
-//                boardViews[3] == (boardViews[4]) && boardViews[4] == (boardViews[5]) ||
-//                boardViews[6] == (boardViews[7]) && boardViews[7] == (boardViews[8]) ||
-//                boardViews[0] == (boardViews[3]) && boardViews[3] == (boardViews[6]) ||
-//                boardViews[1] == (boardViews[4]) && boardViews[4] == (boardViews[7]) ||
-//                boardViews[2] == (boardViews[5]) && boardViews[5] == (boardViews[8]) ||
-//                boardViews[0] == (boardViews[4]) && boardViews[4] == (boardViews[8]) ||
-//                boardViews[2] == (boardViews[4]) && boardViews[4] == (boardViews[6]);
-//    }
-//
-//
-//    private static boolean tie() {
-//        //If there are no more available spaces on the boardNumbers and we've reached gameOver(), it's a tie.
-//        if (currentState == boardViews.length) {
-//            round_text.setText(R.string.tie);
-//            return true;
-//        }
-//        return false;
-//    }
 
 }
--- app/src/main/java/com/example/sweetseedsapp/controllersandviews/DragListener.java
+++ app/src/main/java/com/example/sweetseedsapp/controllersandviews/DragListener.java
@@ -9,16 +9,22 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.util.Log;
+import android.view.DragAndDropPermissions;
 import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.OvershootInterpolator;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sweetseedsapp.R;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by vivianbabiryekulumba on 5/27/18.
@@ -26,91 +32,52 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class DragListener implements View.OnDragListener {
 
-    private GridLayout mGrid;
-    private ScrollView mScrollView;
-    private ValueAnimator mAnimator;
-    private AtomicBoolean mIsScrolling = new AtomicBoolean(false);
+    TextView round_text;
 
     @Override
     public boolean onDrag(View v, DragEvent event) {
-        // Defines a variable to store the action type for the incoming event
         int action = event.getAction();
-        // Handles each of the expected events
         switch (action) {
-
-
             case DragEvent.ACTION_DRAG_STARTED:
-
-
-                // Determines if this View can accept the dragged data
                 if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                    // applies a blue color tint to the View to indicate that it can accept the data
-                    v.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
-                    // Invalidate the view to force a redraw in the new tint
+                    v.getBackground().setColorFilter(v.getResources().getColor(R.color.black_transparent_image),
+                            PorterDuff.Mode.SRC_IN);
                     v.invalidate();
-                    // returns true to indicate that the View can accept the dragged data.
                     return true;
                 }
-                // Returns false. During the current drag and drop operation, this View will
-                // not receive events again until ACTION_DRAG_ENDED is sent.
                 return false;
-
             case DragEvent.ACTION_DRAG_ENTERED:
-
-
-                // Applies a YELLOW or any color tint to the View. Return true; the return value is ignored.
-                v.getBackground().setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_IN);
-                // Invalidate the view to force a redraw in the new tint
                 v.invalidate();
                 return true;
-
-
             case DragEvent.ACTION_DRAG_LOCATION:
-                // Ignore the event
                 return true;
-
-
             case DragEvent.ACTION_DRAG_EXITED:
-
-
-                // Re-sets the color tint to blue, if you had set the BLUE color or any color in ACTION_DRAG_STARTED. Returns true; the return value is ignored.
-                v.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
-                //If u had not provided any color in ACTION_DRAG_STARTED then clear color filter.
                 v.getBackground().clearColorFilter();
-                // Invalidate the view to force a redraw in the new tint
                 v.invalidate();
                 return true;
             case DragEvent.ACTION_DROP:
-                // Gets the item containing the dragged data
                 ClipData.Item item = event.getClipData().getItemAt(0);
-                // Gets the text data from the item.
                 String dragData = item.getText().toString();
-                // Displays a message containing the dragged data.
-//                Toast.makeText(this, .getString(R.string.drag) + dragData, Toast.LENGTH_SHORT).show();
-                // Turns off any color tints
+                // Toast.makeText(this, "Dragged data is " + dragData, Toast.LENGTH_SHORT).show();
+
                 v.getBackground().clearColorFilter();
-                // Invalidates the view to force a redraw
                 v.invalidate();
-                // Returns true. DragEvent.getResult() will return true.
+
+                View view = (View) event.getLocalState();
+                ViewGroup owner = (ViewGroup) view.getParent();
+                owner.removeView(view);
+                LinearLayout container = (LinearLayout) v;
+                container.addView(view);
+                view.setVisibility(View.VISIBLE);
                 return true;
             case DragEvent.ACTION_DRAG_ENDED:
-                // Turns off any color tinting
                 v.getBackground().clearColorFilter();
-                // Invalidates the view to force a redraw
                 v.invalidate();
-                // Does a getResult(), and displays what happened.
-                if (event.getResult()) {
-//                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_SHORT).show();
-//                    Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_SHORT).show();
-                    // returns true; the value is ignored.
-                    return true;
-                }
-                    // An unknown action type was received.
-                    default:
-                        Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
-                        break;
-                }
-                return false;
+            default:
+                Log.e(TAG, "Unknown action type received by OnDragListener.");
+                break;
+        }
+        return false;
     }
 
 }
