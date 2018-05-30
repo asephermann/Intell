d21deaa2e16d24cdf8ea9f279321f723efa2e80d
ViviKulu <babimaji1111@gmail.com>
2018-05-30 17:33:26 -0400

    Updates to games frag

--- app/src/main/java/com/example/sweetseedsapp/fragments/GamesFragment.java
+++ app/src/main/java/com/example/sweetseedsapp/fragments/GamesFragment.java
@@ -4,11 +4,14 @@ package com.example.sweetseedsapp.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.sweetseedsapp.R;
 import com.example.sweetseedsapp.activities.ninetypes.BodilyKinActivity;
@@ -32,9 +35,15 @@ import java.util.List;
  */
 public class GamesFragment extends Fragment {
 
-    GamesModel gamesModel;
-    RecyclerView recyclerView;
-    private List<GamesModel> dataForGames = new ArrayList<>();
+    private static CardView cardView1;
+    private static CardView cardView2;
+    private static CardView cardView3;
+    private static CardView cardView4;
+    private static CardView cardView5;
+    private static CardView cardView6;
+    private static CardView cardView7;
+    private static CardView cardView8;
+    private static CardView cardView9;
 
     public GamesFragment() {
         // Required empty public constructor
@@ -50,68 +59,93 @@ public class GamesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_games, container, false);
-//        recyclerView = view.findViewById(R.id.games_rv);
-//
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
-//        recyclerView.setLayoutManager(layoutManager);
-//        GamesAdapter gamesAdapter = new GamesAdapter(populateGamesFragment());
-//        gamesAdapter.notifyDataSetChanged();
-//        recyclerView.setAdapter(gamesAdapter);
 
-//        switch (gamesAdapter.getItemCount()){
-//            case 0:
-//                Intent intent = new Intent(getActivity(), BodilyKinActivity.class);
-//                startActivity(intent);
-//                break;
-//            case 1:
-//                Intent intent1 = new Intent(getActivity(), SpatialActivity.class);
-//                startActivity(intent1);
-//                break;
-//            case 2:
-//                Intent intent2 = new Intent(getActivity(), LogicalActivity.class);
-//                startActivity(intent2);
-//                break;
-//            case 3:
-//                Intent intent3 = new Intent(getActivity(), NaturalistActivity.class);
-//                startActivity(intent3);
-//                break;
-//            case 4:
-//                Intent intent4 = new Intent(getActivity(), MusicalActivity.class);
-//                startActivity(intent4);
-//                break;
-//            case 5:
-//                Intent intent5 = new Intent(getActivity(), InterpersonalActivity.class);
-//                startActivity(intent5);
-//                break;
-//            case 6:
-//                Intent intent6 = new Intent(getActivity(), IntrapersonalActivity.class);
-//                startActivity(intent6);
-//                break;
-//            case 7:
-//                Intent intent7 = new Intent(getActivity(), LinguisticActivity.class);
-//                startActivity(intent7);
-//                break;
-//            case 8:
-//                Intent intent8 = new Intent(getActivity(), ExistentialActivity.class);
-//                startActivity(intent8);
-//        }
+        //Set the cardViews
+        cardView1 = view.findViewById(R.id.card_view1);
+        cardView2 = view.findViewById(R.id.card_view2);
+        cardView3 = view.findViewById(R.id.card_view3);
+        cardView4 = view.findViewById(R.id.card_view4);
+        cardView5 = view.findViewById(R.id.card_view5);
+        cardView6 = view.findViewById(R.id.card_view6);
+        cardView7 = view.findViewById(R.id.card_view7);
+        cardView8 = view.findViewById(R.id.card_view8);
+        cardView9 = view.findViewById(R.id.card_view9);
+
+        //Set the onClickListeners to go to type activities.
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), BodilyKinActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(), LogicalActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getActivity(), MusicalActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(getActivity(), NaturalistActivity.class);
+                startActivity(intent3);
+            }
+        });
+
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4 = new Intent(getActivity(), ExistentialActivity.class);
+                startActivity(intent4);
+            }
+        });
+
+
+        cardView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent5 = new Intent(getActivity(), IntrapersonalActivity.class);
+                startActivity(intent5);
+            }
+        });
+
+
+        cardView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent6 = new Intent(getActivity(), InterpersonalActivity.class);
+                startActivity(intent6);
+            }
+        });
+
+        cardView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent7 = new Intent(getActivity(), ExistentialActivity.class);
+                startActivity(intent7);
+            }
+        });
+
+        cardView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent8 = new Intent(getActivity(), LinguisticActivity.class);
+                startActivity(intent8);
+            }
+        });
         return view;
     }
 
-//    private List<GamesModel> populateGamesFragment() {
-//        dataForGames.add(new GamesModel(R.drawable.bodily_kin));
-//        dataForGames.add(new GamesModel(R.drawable.spatial));
-//        dataForGames.add(new GamesModel(R.drawable.logical));
-//        dataForGames.add(new GamesModel(R.drawable.naturalist));
-//        dataForGames.add(new GamesModel(R.drawable.musical));
-//        dataForGames.add(new GamesModel(R.drawable.interpersonal));
-//        dataForGames.add(new GamesModel(R.drawable.intrapersonal));
-//        dataForGames.add(new GamesModel(R.drawable.linguistic));
-//        dataForGames.add(new GamesModel(R.drawable.existential));
-//
-//        for (int i = 0; i < dataForGames.size(); i++) {
-//            gamesModel = dataForGames.get(i);
-//        }
-//        return dataForGames;
-//    }
 }
